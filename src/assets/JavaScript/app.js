e5720ea0092eba7dddd2b56c8714fc60dc287b30
HauVal <valentin.hauspie@laposte.net>
2024-04-25 14:34:09 +0200

    ajout du message d'information pour signaler que le mail est bien envoyé

--- src/assets/JavaScript/app.js
+++ src/assets/JavaScript/app.js
@@ -1,5 +1,3 @@
-function displayErrorMessage() {
-
     // Récupération des éléments du formulaire
     let contact = document.getElementById("myForm")
     let inputFirstName = document.getElementById("first-name");
@@ -8,11 +6,18 @@ function displayErrorMessage() {
     let inputObject = document.getElementById("object");
     let inputMessage = document.getElementById("message");
 
+//fonction pour faire apparaitre le message d'erreur ou le message d'information
+function displayMessages() {
     // Vérification que tous les champs sont remplis
     if (inputFirstName.value === "" || inputLastName.value === "" || inputEmail.value === "" || inputObject.value === "" || inputMessage.value === "") {
         // Affichage du message d'erreur
         document.getElementById("error-message").style.display = "block";
+        //Réinitialisation du message d'information s'il était affiché précédemment
+        document.getElementById("information-message").style.display = "none";
         return; // Arrête l'exécution de la fonction si un champ est vide
+    }else{
+        document.getElementById("information-message").style.display = "block";
+        //Affichage du message d'information
     }
 
     // Réinitialisation du message d'erreur s'il était affiché précédemment
@@ -23,4 +28,10 @@ function displayErrorMessage() {
 
 }
 
+//Empèche le formulaire d'actualiser la page
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+});
 
