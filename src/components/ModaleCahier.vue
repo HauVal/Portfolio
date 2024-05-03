67c24006cac581715d1a6246fc722e5124f84c15
HauVal <valentin.hauspie@laposte.net>
2024-05-03 14:53:53 +0200

    creation components presentaiton et creation

--- src/components/ModaleCahier.vue
+++ src/components/ModaleCahier.vue
@@ -1,105 +1,97 @@
 <template>
-
-    <div class="bloc-modale" v-if="revele">
-
+  <div class="bloc-modale" v-if="revele">
     <div class="overlay" v-on:click="toggleModale"></div>
 
     <div class="modale card">
-        <button v-on:click="toggleModale" class="btn-modale btn btn-danger">x</button>
-        <h4>Cahier des charges : La Socketterie</h4>
-        <p>
-          Date de création : 
-          <time datetime="2024-03-20">20 mars 2024</time>
-        </p>
-        <p>Technologie utilisée : Microsoft Word et Canvas</p>
-        <p>
-          <a href="https://www.dropbox.com/scl/fi/xlqc2pw4kft99k0kqqzug/Cahier-des-charges-La-socketterie.pdf?rlkey=ja7pyyovaso3lez1fqprzn57j&st=5enbj0lq&dl=0" target="_blank">
-            Voir le cahier des charges
-          </a>
-        </p>
+      <button v-on:click="toggleModale" class="btn-modale btn btn-danger">
+        x
+      </button>
+      <h4>Cahier des charges : La Socketterie</h4>
+      <p>
+        Date de création :
+        <time datetime="2024-03-20">20 mars 2024</time>
+      </p>
+      <p>Technologie utilisée : Microsoft Word et Canvas</p>
+      <p>
+        <a
+          href="https://www.dropbox.com/scl/fi/xlqc2pw4kft99k0kqqzug/Cahier-des-charges-La-socketterie.pdf?rlkey=ja7pyyovaso3lez1fqprzn57j&st=5enbj0lq&dl=0"
+          target="_blank"
+        >
+          Voir le cahier des charges
+        </a>
+      </p>
     </div>
-
-    </div>
+  </div>
 </template>
 
-
 <script>
-    export default {
-        name: 'modaleCahier',
-        props: ['revele', 'toggleModale']
-    }
+export default {
+  name: "modaleCahier",
+  props: ["revele", "toggleModale"],
+};
 </script>
 
-
 <style scoped>
-
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
-@font-face {
-    font-family: "Canva";
-    src: url('../assets/police/made_canvas/MADE-Canvas-Regular-PERSONAL-USE.woff');
-    src: url('../assets/police/made_canvas/MADE\ Canvas\ Regular\ PERSONAL\ USE.otf') format('opentype');
+.bloc-modale {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-    .bloc-modale {
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+.overlay {
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
 
-    .overlay {
-        background: rgba(0, 0, 0, 0.5);
-        position:fixed;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-    }
+.modale {
+  background: #3d3434;
+  padding: 50px;
+  position: fixed;
+  top: 25%;
+}
 
-    .modale {
-        background: #3d3434;
-        padding: 50px;
-        position: fixed;
-        top: 25%;
-    }
+.modale p a {
+  text-decoration: none;
+  color: #f8f8f8;
+  font-family: "M PLUS Rounded 1c", sans-serif;
+  font-weight: 700;
+}
 
-    .modale p a {
-        text-decoration: none;
-        color: #f8f8f8;
-        font-family: "M PLUS Rounded 1c", sans-serif;
-        font-weight: 700;
-    }
+.modale p a:hover {
+  text-decoration: underline;
+}
 
-    .modale p a:hover {
-        text-decoration: underline;
-    }
+.modal p {
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  line-height: 1.4;
+  margin-bottom: 5px;
+}
 
-    .modal p {
-        font-family: "Poppins", sans-serif;
-        font-weight: 400;
-        font-style: normal;
-        line-height: 1.4;
-        margin-bottom: 5px;
-    }
+h4 {
+  margin-bottom: 10px;
+  font-family: "Canva", serif;
+  font-weight: 700;
+}
 
-    h4 {
-        margin-bottom: 10px;
-        font-family: "Canva", serif;
-        font-weight: 700;
-    }
-
-    .btn-modale {
-        cursor: pointer;
-        background-color: red;
-        color: white;
-        position: absolute;
-        top: 10px;
-        right: 10PX;
-    }
+.btn-modale {
+  cursor: pointer;
+  background-color: red;
+  color: white;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,177 +1,67 @@
 <template>
-      <section id="presentation">
-        <h2>HAUSPIE Valentin</h2>
-  
-        <p>
-          Je m'appelle Hauspie Valentin et j'ai 29 ans. Je suis originaire du Nord
-          et je suis actuellement en formation de Développeur Web. Avant de
-          commencer cette formation, je travaillais dans l'animation auprès des
-          jeunes de 3 à 12 ans en tant qu'animateur des temps périscolaires
-          (cantine, garderie, etc.) et directeur de centre de loisirs. Je suis
-          également musicien, ayant suivi une formation professionnelle aux
-          métiers de la musique. Je donnais également des cours de batterie dans
-          une école de musique. Je suis également le batteur du groupe BlackDog
-          qui joue des reprises de musiques rock/blues des années 70 à 90. J'ai
-          décidé de me lancer dans une carrière de développeur web car cela
-          m'intéresse depuis le lycée et que j'avais toujours hésité entre la
-          musique et le développement web, ce qui va me permettre de pouvoir
-          combiner les deux.
-        </p>
-      </section>
-  
-      <section id="créations">
-        <h2>Mes créations</h2>
-        <div>
-          <!--Balise ajouté pour faciliter la mise en page css-->
-          <article>
-            <h3>Mon CV</h3>
-  
-            <img src="@/assets/images/CV.png" alt="CV rouge et blanc" />
-  
-            <!--Bouton information du modal CV-->
-            <contenuCv></contenuCv>
-          </article>
-  
-          <article>
-            <h3>Mon premier cahier des charges</h3>
-  
-            <img
-              src="@/assets/images/Cahier-des-charges.png"
-              alt="Page de couverture vert et blanc d'un cahier des charges de site web pour l'entreprise la Socketerie"
-            />
-            <!--Bouton information du modal Cahier-->
-            <contenuCahier></contenuCahier>
-          </article>
-  
-          <article>
-            <h3>Espace commentaire dynamisé</h3>
-  
-            <img
-              src="@/assets/images/espace-commentaires.png"
-              alt="image d'un espace commentaire avec plusieurs commentaire afficher et un formulaire d'envoi"
-            />
-            <!--Bouton information du modal espace de commentaire-->
-            <contenuCom></contenuCom>
-          </article>
-        </div>
-      </section>
-  
-      <section id="contact">
-        <formulaire></formulaire>
-      </section>
- 
-  </template>
-  
-  <script setup>
-  
-  import Formulaire from "@/components/Formulaire/Formulaire.vue";
-  import ContenuCv from "@/components/ContenuCv.vue";
-  import ContenuCahier from "@/components/ContenuCahier.vue";
-  import ContenuCom from "@/components/ContenuComment.vue";
-  
-  
-  </script>
-  
-  <style scoped>
-  /*import police*/
-  @import url("https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap");
-  @import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap");
-  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap");
-  @font-face {
-    font-family: "braders";
-    src: url(@/assets/police/braders/Braders.woff);
-    src: url(@/assets/police/braders/Braders.otf) format("opentype");
-  }
-  @font-face {
-    font-family: "Canva";
-    src: url(@/assets/police/made_canvas/MADE-Canvas-Regular-PERSONAL-USE.woff);
-    src: url(@/assets/police/made_canvas/MADE\ Canvas\ Regular\ PERSONAL\ USE.otf)
-      format("opentype");
-  }
-  
-  /*police des h2 et p*/
-  h2 {
-    font-family: "Fjalla One", sans-serif;
-  }
-  
-  p {
-    font-family: "Poppins", sans-serif;
-    font-weight: 400;
-    font-style: normal;
-  }
-  
-  /*style présentation*/
-  #presentation {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  
-  #presentation h2 {
-    padding-top: 150px;
-  }
-  
-  #presentation p {
-    text-align: justify;
-    margin-left: 20px;
-    margin-right: 20px;
-  }
-  
-  /*style mes créations*/
-  #créations {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  #créations h2 {
-    padding-top: 150px;
-  }
-  
-  h3 {
-    font-family: "Canva", serif;
-    font-weight: 700;
-  }
-  
-  #créations img {
-    height: 300px;
-    border-style: solid;
-    border-color: #75949e;
-    margin-left: 50px;
-    margin-right: 50px;
-    margin-bottom: 20px;
-  }
-  
-  #créations img:hover {
-    box-shadow: 15px 15px 40px rgba(150, 146, 146, 0.932);
-  }
-  
-  #créations div {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-between;
-  }
-  
-  #créations article {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  /*ajout pointer sur les boutons*/
-  button {
-    cursor: pointer;
-    font-family: "Poppins", sans-serif;
-    font-weight: 400;
-    font-style: normal;
-  }
-  
-  /*style  contact*/
-  #contact {
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: column;
-    align-items: center;
-  }
-  </style>
+  <section id="presentation">
+    <presentation></presentation>
+  </section>
+
+  <section id="créations">
+    <creation></creation>
+  </section>
+
+  <section id="contact">
+    <formulaire></formulaire>
+  </section>
+</template>
+
+<script setup>
+import Presentation from "@/components/componentsHome/Presentation.vue";
+import Creation from "@/components/componentsHome/Creation.vue";
+import Formulaire from "@/components/componentsHome/Formulaire.vue";
+</script>
+
+<style scoped>
+/*import police*/
+@import url("https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap");
+@font-face {
+  font-family: "braders";
+  src: url(@/assets/police/braders/Braders.woff);
+  src: url(@/assets/police/braders/Braders.otf) format("opentype");
+}
+@font-face {
+  font-family: "Canva";
+  src: url(@/assets/police/made_canvas/MADE-Canvas-Regular-PERSONAL-USE.woff);
+  src: url(@/assets/police/made_canvas/MADE\ Canvas\ Regular\ PERSONAL\ USE.otf)
+    format("opentype");
+}
+
+/*style présentation*/
+#presentation {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+/*style mes créations*/
+#créations {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+/*ajout pointer sur les boutons*/
+button {
+  cursor: pointer;
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+/*style  contact*/
+#contact {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
